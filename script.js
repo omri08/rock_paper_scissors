fe411567845de48a15ab5c7a8f1099e25f704446
Omri Malka <omrimalka08@gmail.com>
2019-10-08 18:40:59 +0300

    added css and update the js, the project is complete

--- script.js
+++ script.js
@@ -1,36 +1,80 @@
-for (let i = 0; i < 5; i++) {
-  game();
+let buttons = document.querySelectorAll("button");
+let updateResult = document.querySelector('.choices-container');
+let wins = 0;
+let ties = 0;
+let losses = 0;
+
+buttons.forEach(button => {
+  button.addEventListener("click", event => {
+    let playerChoice = button.innerHTML;
+    game(playerChoice);
+  });
+});
+
+function game(player) {
+  let pcChoice = pcChooce();
+  whoWins(pcChoice, player);
+  updateMoves(pcChoice,player);
 }
 
-function game() {
-  let playerChoice = window.prompt("choose rock, paper, or scissors", "rock");
-  let pcChoice = (() => {
-    let random = Math.floor(Math.random() * Math.floor(3));
-    switch (random) {
-      case 0:
-        return "rock";
-      case 1:
-        return "paper";
-      case 2:
-        return "scissors";
-    }
-  })();
-
-
-  let result = ((player,computer) =>{
-    console.log(`You choose: ${player} and the computer choose: ${computer}`);
-  if (player === computer) {
-    return "tie";
-  } else if (
-    (player === "rock" && computer === "scissors") ||
-    (player === "paper" && computer === "rock") ||
-    (player === "scissors" && computer === "paper")
+function whoWins(pc, player) {
+  if (
+    (pc === "ROCK" && player === "SCISSORS") ||
+    (pc === "SCISSORS" && player === "PAPER") ||
+    (pc === "PAPER" && player === "ROCK")
   ) {
-    return "You win!";
+    losses++;
+    updateLosses();
+  } else if (pc === player) {
+    ties++;
+    updateTies();
   } else {
-    return "You lose!";
+    wins++;
+    updateWins();
   }
-  });
+}
 
-  console.log(result(playerChoice,pcChoice));
+function updateMoves(pc,player) {
+  let updatePc = document.querySelector('p[class="pc-move"]');
+  let updatePlayer = document.querySelector('p[class="player-move"]');
+
+  updatePc.innerText = `computer choose: ${pc}`;
+  updatePlayer.innerText = `you choose:  ${player}`;
+}
+
+function updateLosses() {
+  let update = document.querySelector('p[class~="losses"]');
+  update.innerText = `lossess: ${losses}`;
+
+  let result = document.querySelector('p[class~="result"]')
+  result.innerHTML = `YOU LOSE!`
+}
+
+function updateTies() {
+  let update = document.querySelector('p[class~="ties"]');
+  update.innerText = `ties: ${ties}`;
+
+  let result = document.querySelector('p[class~="result"]')
+  result.innerHTML = `It's a tie!`
+}
+
+function updateWins() {
+  let update = document.querySelector('p[class~="wins"]');
+  update.innerText = `wins: ${wins}`;
+
+  let result = document.querySelector('p[class~="result"]')
+  result.innerHTML = `YOU WIN!`
+}
+
+function pcChooce() {
+  let int = Math.floor(Math.random() * Math.floor(3));
+
+  switch (int) {
+    case 0:
+      return "ROCK";
+    case 1:
+      return "PAPER";
+    case 2:
+      return "SCISSORS";
+  }
 }
